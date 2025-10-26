83dcd03fd6ea0e56a9cc1ca4ccf12f3350178dfa
Fatt1 <phat3022005@gmail.com>
2025-10-26 20:26:27 +0700

    getAllUsers

---
+++ backend/TrackingBusSystemSolution/TrackingBusSystem.Application/Features/Users/DTOs/GetAllUSersDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TrackingBusSystem.Application.Features.Users.DTOs
+{
+    public record GetAllUSersDTO
+    {
+        public string UserId { get; init; } = null!;
+        public List<string> Roles { get; init; } = null!;
+        public string FullName { get; init; } = null!;
+        public DateOnly DateOfBirth { get; init; }
+    }
+}
